e91d5a385f06225f1a466994a65926837f414d63
NokaAngel <pricetagstudios@gmail.com>
2025-03-31 14:36:45 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,8 +4,8 @@ games { 'gta5' }
 -- Define the resource metadata
 name "Raccoon-Whitelist"
 description "A simple whitelist script that utilizes Badger's Discord API"
-author "TheRaccoon#1023 (PeEcEeChIP)"
-version "v0.1.1.1"
+author "NokaAngel"
+version "v0.1.1.2"
 
 
 server_script "settings.lua"
