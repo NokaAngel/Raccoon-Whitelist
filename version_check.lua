62d72711671ddcebd73d7e34c393d09197a5fe60
NokaAngel <pricetagstudios@gmail.com>
2025-03-31 14:37:36 -0500

    New Version Check System

--- version_check.lua
+++ version_check.lua
@@ -6,42 +6,47 @@ local label =
   ||     / /_/ / __ `/ ___/ ___/ __ \/ __ \/ __ \    | | /| / / __ \/ / __/ _ \/ / / ___/ __/
   ||    / _, _/ /_/ / /__/ /__/ /_/ / /_/ / / / /    | |/ |/ / / / / / /_/  __/ / (__  ) /_  
   ||   /_/ |_|\__,_/\___/\___/\____/\____/_/ /_/     |__/|__/_/ /_/_/\__/\___/_/_/____/\__/  																													
-  ||       				Created by TheRaccoon#1023
+  ||       				Created by TheRaccoon (NokaAngel)
   ||]]  
 Citizen.CreateThread(function()
-	local CurrentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version', 0)
-	if not CurrentVersion then
-		print('^1Raccoon-Whitelist Version Check Failed!^7')
-	end
+    local CurrentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version', 0)
+    if not CurrentVersion then
+        print('^1Raccoon-Whitelist Version Check Failed!^7')
+    end
 
-	function VersionCheckHTTPRequest()
-		PerformHttpRequest('https://raw.githubusercontent.com/PeEcEeChIP/version-history/version/raccoon-whitelist.json', VersionCheck, 'GET')
-	end
+    function VersionCheckHTTPRequest()
+        PerformHttpRequest('https://versions.theraccoon.dev/version.php?resource=raccoon-whitelist', VersionCheck, 'GET')
+    end
 
-	function VersionCheck(err, response, headers)
-		Citizen.Wait(3000)
-		if err == 200 then
-			local Data = json.decode(response)
-			if CurrentVersion ~= Data.NewestVersion then
-				print( label )			
-				print('  ||    \n  ||    Raccoon-Whitelist is outdated!')
-				print('  ||    Current version: ^2' .. Data.NewestVersion .. '^7')
-				print('  ||    Your version: ^1' .. CurrentVersion .. '^7')
-				print('  ||    Please download the lastest version from ^5' .. Data.DownloadLocation .. '^7')
-				if Data.Changes ~= '' then
-					print('  ||    \n  ||    ^5Changes: ^7' .. Data.Changes .. "\n^0  \\\\\n")
-				end
-			else
-				print( label )			
-				print('  ||    ^2Raccoon-Whitelist is up to date!\n^0  ||\n  \\\\\n')
-			end
-		else
-			print( label )			
-			print('  ||    ^1There was an error getting the latest version information, if the issue persists contact TheRaccoon#1023 on Discord.\n^0  ||\n  \\\\\n')
-		end
-		
-		SetTimeout(60000000, VersionCheckHTTPRequest)
-	end
+    function VersionCheck(err, response, headers)
+        Citizen.Wait(3000)
+        if err == 200 then
+            local version, download, changes = response:match('([^|]+)|([^|]+)|(.+)')
+            if not version or version == 'error' then
+                print(label)
+                print('  ||    ^1Error fetching version data from server.\n^0  ||\n  \\\\\n')
+                return
+            end
+            if CurrentVersion ~= version then
+                print(label)
+                print('  ||    \n  ||    Raccoon-Whitelist is outdated!')
+                print('  ||    Current version: ^2' .. version .. '^7')
+                print('  ||    Your version: ^1' .. CurrentVersion .. '^7')
+                print('  ||    Please download the latest version from ^5' .. download .. '^7')
+                if changes ~= '' then
+                    print('  ||    \n  ||    ^5Changes: ^7' .. changes .. "\n^0  \\\\\n")
+                end
+            else
+                print(label)
+                print('  ||    ^2Raccoon-Whitelist is up to date!\n^0  ||\n  \\\\\n')
+            end
+        else
+            print(label)
+            print('  ||    ^1There was an error getting the latest version information, if the issue persists contact TheRaccoon#1023 on Discord.\n^0  ||\n  \\\\\n')
+        end
+        
+        SetTimeout(60000, VersionCheckHTTPRequest)
+    end
 
-	VersionCheckHTTPRequest()
+    VersionCheckHTTPRequest()
 end)
